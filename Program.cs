69a899976b205672db93beef4e89c6a40e2144ef
Serg <serge-malov@yandex.ru>
2023-10-03 16:03:49 +0300

    Solution v 1.1 added

--- Program.cs
+++ Program.cs
@@ -3,36 +3,72 @@
 // либо задать на старте выполнения алгоритма. При решении не рекомендуется пользоваться коллекциями, 
 // лучше обойтись исключительно массивами.
 
-Console.Write("Введите размер массива: ");
-int size = int.Parse(Console.ReadLine()!);
-string[] stringsArray = new string[size];
-int lessThan3Counter = 0;
-Console.WriteLine("Введите строки: ");
-
-for (int i = 0; i < size; i++)
-    stringsArray[i] = Console.ReadLine()!;
-
-for (int i = 0; i < size; i++)
+int InputArraySize()
 {
-    if (stringsArray[i].Length < 4)
-        lessThan3Counter++;
-}
-
-string[] resultString = new string[lessThan3Counter];
-lessThan3Counter = 0;
-
-for (int i = 0; i < size; i++)
-{
-    if (stringsArray[i].Length < 4)
+    int number = 0;
+    while (true)
     {
-        resultString[lessThan3Counter] = stringsArray[i];
-        lessThan3Counter++;
+        Console.Write("Введите размер массива: ");
+        try
+        {
+            number = int.Parse(Console.ReadLine()!);
+            if (number > 0)
+            {
+                break;
+            }
+            Console.WriteLine("\nРазмер массива не может быть отрицательным числом. Пожалуйста повторите ввод.\n");
+        }
+        catch (Exception)
+        {
+            Console.WriteLine("\nНеверный ввод. Пожалуйста повторите ввод.\n");
+        }
     }
+    return number;
 }
 
-Console.WriteLine("\nИтоговый массив: ");
-
-for (int i = 0; i < lessThan3Counter; i++)
+string[] CreateArray(int size)
 {
-    Console.WriteLine(resultString[i]);
+    string[] array = new string[size];
+    Console.WriteLine($"Введите {size} строк массива: ");
+
+    for (int i = 0; i < size; i++)
+        array[i] = Console.ReadLine()!;
+
+    return array;
 }
+
+string[] GetLessThanThreeSymbolStrings(string[] array)
+{
+    int counter = 0;
+    foreach (string s in array)
+    {
+        if (s.Length < 4)
+            counter++;
+    }
+
+    string[] result = new string[counter];
+    counter = 0;
+
+    foreach (string s in array)
+    {
+        if (s.Length < 4)
+        {
+            result[counter] = s;
+            counter++;
+        }
+    }
+
+    return result;
+}
+
+void PrintArray(string[] array)
+{
+    foreach (string s in array)
+        Console.WriteLine(s);
+}
+
+int size = InputArraySize();
+string[] stringsArray = CreateArray(size);
+string[] resultString = GetLessThanThreeSymbolStrings(stringsArray);
+Console.WriteLine("\nВ исходном массиве следующие строки имеют длину меньше либо равны 3-м символам:");
+PrintArray(resultString);
